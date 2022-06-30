4b7957e29bc9f517bb2f428cc1139a88b5fbf3ea
AlbertClemente <aclemente.dev@gmail.com>
2022-06-30 14:44:34 +0200

    edit crud

--- edit.php
+++ edit.php
@@ -1,25 +1,41 @@
 <?php
-  require "database.php";
+require "database.php";
 
-  $error = null;
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["name"]) || empty($_POST["phone_number"])) {
-      $error = "Please, fill all the fields.";
-    } else if (strlen($_POST["phone_number"]) < 9) {
-      $error = "Phone number must be at least 9 characters long";
-    } else {
-      $name = $_POST["name"];
-      $phoneNumber = $_POST["phone_number"];
+$id = $_GET["id"];
 
-      $statement = $connection->prepare("INSERT INTO contacts (name, phone_number) VALUES(:name, :phone_number);");
-      $statement -> bindParam(":name", $_POST["name"]);
-      $statement -> bindParam(":phone_number", $_POST["phone_number"]);
+$statement = $connection->prepare("SELECT * FROM contacts WHERE id = :id LIMIT 1;");
+$statement->bindParam(":id", $id);
+$statement->execute();
 
-      $statement->execute();
+if ($statement->rowCount() == 0) {
+  http_response_code(404);
+  echo ("<h1>HTTP 404 NOT FOUND :(");
+  return;
+}
 
-      header("Location: index.php");
-    }
+$contact = $statement->fetch(PDO::FETCH_ASSOC);
+
+$error = null;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"]) || empty($_POST["phone_number"])) {
+    $error = "Please, fill all the fields.";
+  } else if (strlen($_POST["phone_number"]) < 9) {
+    $error = "Phone number must be at least 9 characters long";
+  } else {
+    $name = $_POST["name"];
+    $phoneNumber = $_POST["phone_number"];
+
+    $statement = $connection->prepare("UPDATE contacts SET name = :name, phone_number = :phone_number WHERE id = :id;");
+    $statement->bindParam(":id", $id);
+    $statement->bindParam(":name", $_POST["name"]);
+    $statement->bindParam(":phone_number", $_POST["phone_number"]);
+
+    $statement->execute();
+
+    header("Location: index.php");
   }
+}
 ?>
 
 
@@ -66,21 +82,21 @@
           </div>
         <?php endif ?>
 
-        <h2 class="mb-5">Add Contact</h2>
-        <form method="POST" action="add.php">
+        <h2 class="mb-5">Edit contact <?= $contact["name"] ?></h2>
+        <form method="POST" action="edit.php?id=<?= $contact["id"] ?>">
           <div class="row mb-3">
             <label for="name" class="col-sm-2 col-form-label">Contact Name</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" id="name" name="name" required autocomplete="name" autofocus>
+              <input value="<?= $contact["name"] ?>" type="text" class="form-control" id="name" name="name" required autocomplete="name" autofocus>
             </div>
           </div>
           <div class="row mb-3">
             <label for="phone_number" class="col-sm-2 col-form-label">Phone Number</label>
             <div class="col-sm-10">
-              <input type=" text" class="form-control" id="phone_number" name="phone_number" required autocomplete="phone_number" autofocus>
+              <input value="<?= $contact["phone_number"] ?>" type=" text" class="form-control" id="phone_number" name="phone_number" required autocomplete="phone_number" autofocus>
             </div>
           </div>
-          <button type="submit" class="btn btn-primary">Add contact</button>
+          <button type="submit" class="btn btn-primary">Update contact</button>
         </form>
       </div>
     </div>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-  require "database.php";
+require "database.php";
 
-  $contacts = $connection -> query("SELECT * FROM contacts");
+$contacts = $connection->query("SELECT * FROM contacts");
 ?>
 
 <!DOCTYPE html>
@@ -40,7 +40,7 @@
   <main>
     <div class="container pt-5 p-3">
       <div class="row row-cols-4">
-        <?php if ($contacts -> rowCount() == 0) : ?>
+        <?php if ($contacts->rowCount() == 0) : ?>
           <div class="col-12 mb-5">
             <div class="card text-center">
               <div class="card-body">
@@ -57,7 +57,7 @@
               <div class="card-body">
                 <h3 class="card-title text-capitalize"><?= $contact["name"] ?></h3>
                 <p class="card-text m-2"><?= $contact["phone_number"] ?></p>
-                <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
+                <a href="edit.php?id=<?= $contact["id"] ?>" class="btn btn-secondary mb-2">Edit Contact</a>
                 <a href="delete.php?id=<?= $contact["id"] ?>" class="btn btn-danger mb-2">Delete Contact</a>
               </div>
             </div>
